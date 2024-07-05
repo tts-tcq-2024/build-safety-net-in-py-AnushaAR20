15f3d95c254dab05f196a15b2a62acf92ea1269c
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-05 15:26:52 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -22,7 +22,7 @@ class TestSoundex(unittest.TestCase):
                 self.assertEqual(generate_soundex("b#tg"), "B320")
 
     def test_word3(self):
-            self.assertEqual(generate_soundex("LLR"), "L460")
+            self.assertEqual(generate_soundex("LALR"), "L046")
         
     def empty(self):
                 self.assertEqual(generate_soundex(""), "")
