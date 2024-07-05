6ec996b42227ce2c7ff82ae454a758f21169e168
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-05 15:44:28 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -12,8 +12,8 @@ class TestSoundex(unittest.TestCase):
     def test_vowels(self):
             self.assertEqual(generate_soundex("AEIOU"), "A000")
     
-    # def test_specialchar(self):
-    #         self.assertEqual(generate_soundex("#"), "#000")
+    def test_specialchar(self):
+            self.assertEqual(generate_soundex("#"), "#000")
         
     # def test_word(self):
     #         self.assertEqual(generate_soundex("Pond"), "P530")
