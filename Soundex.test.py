896ca95b30c8f53a9c8d5207e831122a479bb479
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-12 14:31:15 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -3,26 +3,20 @@ from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
-    # def test_empty_string(self):
-    #     self.assertEqual(generate_soundex(""), "")
-
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
     
-    # def test_vowels(self):
-    #     self.assertEqual(generate_soundex("AEIOU"), "A000")
-    
-    def test_specialchar(self):
+    def test_special_character(self):
         self.assertEqual(generate_soundex("#"), "#000")
         
     def test_word(self):
         self.assertEqual(generate_soundex("Ppndf"), "P531")
 
-    def test_word3(self):
+    def test_word2(self):
         self.assertEqual(generate_soundex("Lbrl"), "L164")
         
-    # def empty(self):
-    #     self.assertEqual(generate_soundex(""), "")
+    def empty(self):
+        self.assertEqual(generate_soundex(""), "0000")
 
     def empty(self):
         self.assertEqual(generate_soundex("Ltcvnrl"), "L321")
